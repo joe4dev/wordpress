628686f2a51fa075c2d733a6bdfbd41352c517ea
Joel Scheuner <joel.scheuner.dev@gmail.com>
2017-01-19 15:08:40 +0100

    Enable wordpress apache config

--- recipes/apache.rb
+++ recipes/apache.rb
@@ -27,3 +27,12 @@ unless platform? "windows"
 end
 
 include_recipe "wordpress::app"
+
+web_app "wordpress" do
+  template "wordpress.conf.erb"
+  docroot node['wordpress']['dir']
+  server_name node['wordpress']['server_name']
+  server_aliases node['wordpress']['server_aliases']
+  server_port node['wordpress']['server_port']
+  enable true
+ end
